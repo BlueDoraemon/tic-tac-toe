b68c8cb5ed356217843bddee6bf2933796ed30cf
BlueDoraemon <3803480+BlueDoraemon@users.noreply.github.com>
2023-06-01 00:21:24 +1000

    3 Modules added functions for boards and games private for all variables WIP

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,30 @@
 'use strict';
 
-const gameBoard = () => {
-    let _gameboard = [];
+//Player factory function
+function createPlayer(name,symbol){
 
-    let _symbol = 'X';
-        
-    
-    //2D Array
+    let points = 0;
+    function getName(){
+        return name;
+    }
+    function getSymbol(){
+        return symbol;
+    }
+    function winGame(){
+        points++;
+    }
+    function getPoints(){
+        return points;
+    }
+    return {
+        getName, getSymbol, winGame, getPoints
+    }
+}
+
+//gameBoard Module
+const gameBoard = (function() {
+    let _gameboard = [];
+    //2D Array at the moment
     function resetBoard(){
         _gameboard = [
             ['','',''],
@@ -14,26 +32,78 @@ const gameBoard = () => {
             ['','','']];
     };
 
-    function addSymbol(i,j){
-        _gameboard[j][i] = `${_symbol}`;
+    function addSymbol(i,j, symbol){
+        _gameboard[j][i] = `${symbol}`;
     };
 
     function viewGameBoard(){
         console.log(_gameboard);
     };
+    function checkWin(player){ //Object
+
+        //check rows
+        const isSymbol = (symbol) => symbol === player.getSymbol();
+
+        for (let j = 0; j < _gameboard.length; j++){
+            if (_gameboard[j].every(isSymbol)) {
+                console.log(`${player.getName()} has won this round`)
+            }
+        }
+        //check columns
+        for (let i = 0; i < _gameboard.length; i++){
+            _gameboard.map();// WIP
+        }
+        //check diagonals
+    }
  
-   return {viewGameBoard, addSymbol,resetBoard};
-}
+   return {viewGameBoard, addSymbol,resetBoard, checkWin};
+})();
 
-//Player object
-function Player (name,symbol){
-    this.name;
-    this.symbol;
-}
 
+
+
+
+
+//Control the game Module
+
+const displayController = (()=>{
+    
+
+    //init
+     //best of 5?
+     let gameNo = 0;
+    gameBoard.resetBoard();
+ 
+
+    // Rules of the game: 3 symbols in a row wins the game. 
+    //check rows
+
+
+    //Cannot place symbol on an occupied square.
+
+    //checkWin
+
+    //render
+    return {
+
+    };
+})();
 
 // Object.create();
-let game = gameBoard();
-game.resetBoard();
-game.addSymbol(0,2);
-game.viewGameBoard();
+
+// Testing ------------------
+
+
+let player1 = createPlayer('bob','X');
+let player2 = createPlayer('greg','O');
+
+gameBoard.resetBoard();
+gameBoard.addSymbol(0,2,player1.getSymbol());
+
+gameBoard.addSymbol(1,2,player1.getSymbol());
+
+gameBoard.addSymbol(2,2,player1.getSymbol());
+gameBoard.checkWin(player1);
+gameBoard.viewGameBoard();
+player1.symbol = 'y';
+console.log(player1.getSymbol()); //Expect return 'X'
