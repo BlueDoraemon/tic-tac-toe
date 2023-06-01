feeff9c7938a8549fb90d314fa5b49b512418bb0
BlueDoraemon <3803480+BlueDoraemon@users.noreply.github.com>
2023-06-01 20:25:02 +1000

    Finished checkWin using map and every array methods

--- scripts/script.js
+++ scripts/script.js
@@ -25,6 +25,7 @@ function createPlayer(name,symbol){
 const gameBoard = (function() {
     let _gameboard = [];
     //2D Array at the moment
+
     function resetBoard(){
         _gameboard = [
             ['','',''],
@@ -39,21 +40,44 @@ const gameBoard = (function() {
     function viewGameBoard(){
         console.log(_gameboard);
     };
-    function checkWin(player){ //Object
+
+    function checkWin(player){ //Obj
+
+        const isSymbol = (char) => char === player.getSymbol();
+
 
         //check rows
-        const isSymbol = (symbol) => symbol === player.getSymbol();
-
         for (let j = 0; j < _gameboard.length; j++){
             if (_gameboard[j].every(isSymbol)) {
-                console.log(`${player.getName()} has won this round`)
+                console.log(`${player.getName()} has won this round; j = ${j}`)
             }
         }
         //check columns
+        function _extractColumn(_arr, _column){
+        return _arr.map((x)=>x[_column]);
+        }
+
         for (let i = 0; i < _gameboard.length; i++){
-            _gameboard.map();// WIP
+            if (_extractColumn(_gameboard,i).every(isSymbol)) {
+                console.log(`${player.getName()} has won this round; i = ${i}`)
+            }
         }
         //check diagonals
+        let _diagDown = [_gameboard[0][0],_gameboard[1][1],_gameboard[2][2]];
+        let _diagUp = [_gameboard[2][0],_gameboard[1][1],_gameboard[0][2]];
+        if (_diagDown.every(isSymbol)) {
+            console.log(`${player.getName()} has won this round; /`)
+        }
+        if (_diagUp.every(isSymbol)) {
+            console.log(`${player.getName()} has won this round; diagDown`)
+        }
+
+        //console.log(_diagDown,_diagUp); testing
+    }
+
+    // input return coordinates and player won
+    function displayWin(){
+
     }
  
    return {viewGameBoard, addSymbol,resetBoard, checkWin};
@@ -76,8 +100,6 @@ const displayController = (()=>{
  
 
     // Rules of the game: 3 symbols in a row wins the game. 
-    //check rows
-
 
     //Cannot place symbol on an occupied square.
 
@@ -103,6 +125,8 @@ gameBoard.addSymbol(0,2,player1.getSymbol());
 gameBoard.addSymbol(1,2,player1.getSymbol());
 
 gameBoard.addSymbol(2,2,player1.getSymbol());
+
+
 gameBoard.checkWin(player1);
 gameBoard.viewGameBoard();
 player1.symbol = 'y';
